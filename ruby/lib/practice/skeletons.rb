15a334c8211e5ac3e25822e6ee068d64e10e61ca
Diego Guerra <diego.guerra.suarez@gmail.com>
2018-10-28 18:26:27 +0100

    Add initial skeleton for ruby

---
+++ ruby/lib/practice/skeletons.rb
@@ -0,0 +1,7 @@
+require "practice/skeletons/version"
+
+module Practice
+  module Skeletons
+    # Your code goes here...
+  end
+end
---
+++ ruby/lib/practice/skeletons/version.rb
@@ -0,0 +1,5 @@
+module Practice
+  module Skeletons
+    VERSION = "0.1.0".freeze
+  end
+end
