b8da77ec7140a1192cb9e1482f988d97a52d7d4e
Daniel Machado Fernández <danielmfern@ext.zara.com>
2019-12-31 12:58:37 +0100

    Update gradle to 6.0.1, use JUnit Jupiter (Junit 5) as default testing framework

--- java/src/test/java/AppTest.java
+++ java/src/test/java/AppTest.java
@@ -1,8 +1,9 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test 
+    public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
